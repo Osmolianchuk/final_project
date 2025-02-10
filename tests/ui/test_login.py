aa3aeec3f65179a7cc30c0670214845c9b63f4f8
Osmolianchuk <42710899+Osmolianchuk@users.noreply.github.com>
2025-02-10 07:12:21 +0100

    Update test_login.py

--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -19,6 +19,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import tempfile
+import os
+import shutil
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 # Setup basic configuration for logging
 logging.basicConfig(level=logging.INFO, filename='test_log.log', filemode='w',
